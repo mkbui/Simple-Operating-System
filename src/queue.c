f380076fd4b40f45bfedc607bb529affc6d2d158
Voòng Xày Tạc <55225434+tacvoong@users.noreply.github.com>
2020-06-23 20:49:08 +0700

    Update queue.c

--- src/queue.c
+++ src/queue.c
@@ -6,11 +6,13 @@ int empty(struct queue_t * q) {
 }
 
 void enqueue(struct queue_t * q, struct pcb_t * proc) {
-	/* TODO: put a new process to queue [q] */	
+	/* TODO: put a new process to queue [q] */
+	// Check if queue is empty. If it is, add proc in as first element.
 	if(empty(q)){
 		q->proc[0] = proc;
 		q->size=1;
 	}
+	// Else, add proc in right after the last element of the queue.
 	else if(q->size < 10){
 		q->proc[q->size] = proc;
 		q->size++;
@@ -21,23 +23,29 @@ struct pcb_t * dequeue(struct queue_t * q) {
 	/* TODO: return a pcb whose prioprity is the highest
 	 * in the queue [q] and remember to remove it from q
 	 * */
+	//Check if queue is empty. If it is, return nothing.
 	if(empty(q)){
 		return NULL;
 	}
+	//an integer to check the position of the highest priority pcb in the queue
 	int check = 0;
+	//the return pcb
 	struct pcb_t * proc = q->proc[0];
+	//Iterate through the queue to find the pcb with highest priority. If there are more than one with same highest, choose the first one.
+	//Check the position of the selected pcb
 	for(int i = 0; i < q->size; i++){
 		if(q->proc[i]->priority > proc->priority){
 			check = i;
 			proc = q->proc[i];
 		}
 	}
-	
+	//Delete the marked position in the queue, adjust the queue
 	for(int i = check; i< q->size; i++){
 			q->proc[i] = q->proc[i+1];
 	}
 	q->proc[q->size] = NULL;
 	q->size--;
+	//return the selected pcb
 	return proc;
 	
 }
